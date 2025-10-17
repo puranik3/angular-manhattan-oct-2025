554aa31b362e48a77d9521e4e22e52c87bdd9e21
Prashanth Puranik <puranik@digdeeper.in>
2025-10-17 13:07:28 +0530

    Till Step 11

--- demos/02-angular/workshops-app/src/app/workshops/workshops-list/workshops-list.ts
+++ demos/02-angular/workshops-app/src/app/workshops/workshops-list/workshops-list.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgbAlert } from '@ng-bootstrap/ng-bootstrap';
+import { Workshops } from '../workshops';
+import IWorkshop from '../models/IWorkshop';
 
 @Component({
-  selector: 'app-workshops-list',
-  imports: [],
-  templateUrl: './workshops-list.html',
-  styleUrl: './workshops-list.scss'
+    selector: 'app-workshops-list',
+    imports: [CommonModule, NgbAlert],
+    templateUrl: './workshops-list.html',
+    styleUrl: './workshops-list.scss',
 })
-export class WorkshopsList {
+export class WorkshopsList implements OnInit {
+    workshops!: IWorkshop[];
+    error!: Error;
+    loading = true;
 
+    constructor(private w: Workshops) {}
+
+    ngOnInit() {
+        this.loading = true;
+
+        this.w.getWorkshops().subscribe({
+            next: (workshops) => {
+                this.workshops = workshops;
+                this.loading = false;
+            },
+            error: (error) => {
+                this.error = error;
+                this.loading = false;
+            },
+        });
+    }
 }
